89dd9e4df2798fdd660bfcac0085fa91259b1678
RouQandel <rawanqandel62@gmail.com>
2025-04-02 22:06:38 +0200

    Update NavBar

--- PharmaHub/src/app/Shared/NavBar/nav-bar.component.ts
+++ PharmaHub/src/app/Shared/NavBar/nav-bar.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css'] 
 })
 export class NavBarComponent {
 
