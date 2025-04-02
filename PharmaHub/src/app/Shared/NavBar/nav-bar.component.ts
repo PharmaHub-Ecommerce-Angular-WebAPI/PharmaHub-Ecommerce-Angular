8a0ca2f584bcdaa1555cd77a3118402e99d4dd43
RouQandel <rawanqandel62@gmail.com>
2025-04-02 22:01:13 +0200

    Add NavBar section

---
+++ PharmaHub/src/app/Shared/NavBar/nav-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+
+}
