a7b2dcaa0fc7f5b93dd12ba482436528c7ebbbf0
RouQandel <rawanqandel62@gmail.com>
2025-04-05 14:57:09 +0200

    Add home nav bar

---
+++ PharmaHub/src/app/Shared/NavBar/home-nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css'] 
+})
+export class NavBarComponent {
+
+}
