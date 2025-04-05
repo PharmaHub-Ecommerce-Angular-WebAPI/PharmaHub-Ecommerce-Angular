bdf47c3f9aadd4d633c6d19736e6574dcdfcb633
RouQandel <rawanqandel62@gmail.com>
2025-04-05 16:11:04 +0200

    update app com

--- PharmaHub/src/app/app.component.ts
+++ PharmaHub/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './Shared/NavBar/nav-bar.component';
-import { FooterComponent } from './Shared/Footer/footer.component';
+import { NavBarComponent } from './shared/navbar/home-nav-bar/nav-bar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavBarComponent ,FooterComponent],
+  imports: [RouterOutlet , NavBarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
