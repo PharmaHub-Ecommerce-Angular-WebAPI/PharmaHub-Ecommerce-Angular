0e220506c9dc673978ca84dd4c24e84da87c1d68
RouQandel <rawanqandel62@gmail.com>
2025-04-05 14:58:37 +0200

    Add customer nav bar

---
+++ PharmaHub/src/app/Shared/NavBar/customer-nav-bar/customer-nav-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-nav-bar',
+  imports: [],
+  templateUrl: './customer-nav-bar.component.html',
+  styleUrl: './customer-nav-bar.component.css'
+})
+export class CustomerNavBarComponent {
+
+}
