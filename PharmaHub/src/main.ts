8c40d66b4a8ff6611302c7d14b23c4b91070cb70
RouQandel <rawanqandel62@gmail.com>
2025-04-06 01:48:46 +0200

    updated main.ts

--- PharmaHub/src/main.ts
+++ PharmaHub/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+// main.ts
+import { defineElement } from 'lord-icon-element';
+import lottie from 'lottie-web';
 
+// Initialize before bootstrap
+defineElement(lottie.loadAnimation);
+
+// Then bootstrap your app as normal
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch(err => console.error(err));
+
+
