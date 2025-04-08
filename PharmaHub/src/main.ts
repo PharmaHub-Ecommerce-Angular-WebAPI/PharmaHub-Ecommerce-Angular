d07b3997cee6e333922b204e4065d5c109e895c8
RouQandel <rawanqandel62@gmail.com>
2025-04-08 10:30:14 +0200

    Update Main.ts

--- PharmaHub/src/main.ts
+++ PharmaHub/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
 // main.ts
 import { defineElement } from 'lord-icon-element';
 import lottie from 'lottie-web';
@@ -12,4 +13,7 @@ defineElement(lottie.loadAnimation);
 bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
 
+
+
+  
 
