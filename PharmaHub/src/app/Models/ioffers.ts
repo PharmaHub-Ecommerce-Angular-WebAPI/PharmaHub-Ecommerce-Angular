00b4d9bf4495e9187671d3fe0cc53061ede41cc9
eman149 <emanabdou506@gmail.com>
2025-04-11 23:50:31 +0200

    Add Ioffers interface to define offer properties

---
+++ PharmaHub/src/app/Models/ioffers.ts
@@ -0,0 +1,10 @@
+export interface Ioffers {
+    id : number ;
+    name :string ;
+    imgUrl : string ;
+    price : number ;
+    DissPrice : number ;
+    description : string[];
+    pharmName : string ;
+    imgPharm : string;
+}
