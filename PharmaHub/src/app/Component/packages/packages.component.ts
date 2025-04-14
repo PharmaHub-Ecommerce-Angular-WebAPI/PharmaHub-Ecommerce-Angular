618a05fbd0544f294c699fb9460487e2192b2cd2
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-15 00:06:30 +0200

    Refactor PackagesComponent for improved code structure, readability, and package management

--- PharmaHub/src/app/Component/packages/packages.component.ts
+++ PharmaHub/src/app/Component/packages/packages.component.ts
@@ -9,64 +9,62 @@ import { Ipackage } from '../../Models/ipackage';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [FormsModule, CommonModule],
   templateUrl: './packages.component.html',
-  styleUrls: ['./packages.component.css']
+  styleUrls: ['./packages.component.css'],
 })
 export class PackagesComponent {
-
-   packages : Ipackage[] ;
+  packages: Ipackage[];
   currentIndex = 0;
-  
-  constructor(){
+
+  constructor() {
     this.packages = [
-    { imgUrl: './logo.png', name: 'Card 1', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
-    // { imgUrl: './logo.png', name: 'Card 2', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ] , id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
-    // { imgUrl: './logo.png', name: 'Card 3', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
-    // { imgUrl: './logo.png', name: 'Card 4', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
-    // { imgUrl: './logo.png', name: 'Card 5', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
-    // { imgUrl: './logo.png', name: 'Card 6', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
-    // { imgUrl: './logo.png', name: 'Card 7', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
-    // { imgUrl: './logo.png', name: 'Card 8', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
-    // { imgUrl: './logo.png', name: 'Card 9', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
-    // { imgUrl: './logo.png', name: 'Card 10', description : ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ] , id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
-    
-  ]
-}
-
-
-
-
-get limitedPackages() {
-  return this.packages.slice(0, 6);
-}
-
-
-get visibleCards() {
-  return this.limitedPackages.slice(this.currentIndex, this.currentIndex + 3);
-}
-
-
-
-
-
-moveLeft() {
-  if (this.currentIndex > 0) {
-    this.currentIndex--;
-  } else {
-    
-    this.currentIndex = Math.max(this.limitedPackages.length - 3, 0);
+      {
+        imgUrl: './logo.png',
+        name: 'Card 1',
+        description: [
+          'This is compnet 1',
+          'This is compnet 2',
+          'This is compnet 3',
+        ],
+        id: 1,
+        pharmName: 'tobgy',
+        price: 1555,
+        imgPharm: './logo.png',
+      },
+      // { imgUrl: './logo.png', name: 'Card 2', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ] , id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
+      // { imgUrl: './logo.png', name: 'Card 3', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
+      // { imgUrl: './logo.png', name: 'Card 4', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
+      // { imgUrl: './logo.png', name: 'Card 5', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
+      // { imgUrl: './logo.png', name: 'Card 6', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
+      // { imgUrl: './logo.png', name: 'Card 7', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
+      // { imgUrl: './logo.png', name: 'Card 8', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
+      // { imgUrl: './logo.png', name: 'Card 9', description :  ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ], id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
+      // { imgUrl: './logo.png', name: 'Card 10', description : ['This is compnet 1' , 'This is compnet 2' ,'This is compnet 3' ] , id : 1 , pharmName: 'tobgy' , price : 1555 , imgPharm : './logo.png'  },
+    ];
   }
-  console.log(this.packages[this.currentIndex].description )
-}
 
-    moveRight() {
-      if (this.currentIndex < this.limitedPackages.length - 3) {
-        this.currentIndex++;
-      } else {
-        this.currentIndex = 0;
-      }
-      
+  get limitedPackages() {
+    return this.packages.slice(0, 6);
+  }
 
+  get visibleCards() {
+    return this.limitedPackages.slice(this.currentIndex, this.currentIndex + 3);
+  }
+
+  moveLeft() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = Math.max(this.limitedPackages.length - 3, 0);
+    }
+    console.log(this.packages[this.currentIndex].description);
+  }
+
+  moveRight() {
+    if (this.currentIndex < this.limitedPackages.length - 3) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
     }
   }
-  
-
+  isFlipped: boolean = false;
+}
