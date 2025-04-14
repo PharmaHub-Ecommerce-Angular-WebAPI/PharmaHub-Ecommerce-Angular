8c976c1b0f5fe79745697ec2a9402681cafde11a
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-15 00:03:35 +0200

    Refactor app.component.ts to include CustomerComponent and ViewAllPharmaciesComponent in imports

--- PharmaHub/src/app/app.component.ts
+++ PharmaHub/src/app/app.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './Shared/Footer/footer.component';
 import { NavBarComponent } from './Shared/NavBar/nav-bar.component';
-
-
-
+import { CustomerComponent } from './Component/customer/customer.component';
+import { ViewAllPharmaciesComponent } from './Component/view-all-pharmacies/view-all-pharmacies.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet ,NavBarComponent, FooterComponent ],
+  imports: [
+    RouterOutlet,
+    NavBarComponent,
+    FooterComponent,
+    CustomerComponent,
+    ViewAllPharmaciesComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'PharmaHub';
