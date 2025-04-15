42b8c9e5456e264ec58226076a2408730613d712
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-15 22:39:59 +0200

    Add isFlipped property to Ipackage interface for card state management

--- PharmaHub/src/app/Models/ipackage.ts
+++ PharmaHub/src/app/Models/ipackage.ts
@@ -1,9 +1,10 @@
 export interface Ipackage {
-    id : number ;
-    name :string ;
-    imgUrl : string ;
-    price : number ;
-    description : string[];
-    pharmName : string ;
-    imgPharm : string;
+  id: number;
+  name: string;
+  imgUrl: string;
+  price: number;
+  description: string[];
+  pharmName: string;
+  imgPharm: string;
+  isFlipped: boolean;
 }
