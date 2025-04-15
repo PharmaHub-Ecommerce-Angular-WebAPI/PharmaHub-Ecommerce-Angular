d4bb19166918b3d40abcabff1fb855f4921a044e
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-15 22:39:06 +0200

    Add card flipping functionality to display packages component

--- PharmaHub/src/app/Component/display-packages/display-packages.component.ts
+++ PharmaHub/src/app/Component/display-packages/display-packages.component.ts
@@ -11,7 +11,7 @@ import { PharmNameService } from '../../services/pharm-name.service';
   imports: [FormsModule, CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './display-packages.component.html',
-  styleUrl: './display-packages.component.css'
+  styleUrl: './display-packages.component.css',
 })
 export class DisplayPackagesComponent {
   allPackages: Ipackage[] = [];
@@ -26,33 +26,152 @@ export class DisplayPackagesComponent {
     private pharmNameService: PharmNameService
   ) {
     this.allPackages = [
-      { imgUrl: './logo.png', name: 'Card 1', description: ['comp 1'], id: 1, pharmName: 'keky', price: 2000, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 2', description: ['comp 1'], id: 2, pharmName: 'tarshoby', price: 1500, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 3', description: ['comp 1'], id: 3, pharmName: 'almeldin', price: 4000, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 4', description: ['comp 1'], id: 4, pharmName: 'weso', price: 1926, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 5', description: ['comp 1'], id: 5, pharmName: 'khaled', price: 400, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 6', description: ['comp 1'], id: 6, pharmName: 'mahdy', price: 100, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 7', description: ['comp 1'], id: 7, pharmName: 'rewan', price: 600, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 8', description: ['comp 1'], id: 8, pharmName: 'emo', price: 800, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 9', description: ['comp 1'], id: 9, pharmName: 'roby', price: 500, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 10', description: ['comp 1'], id: 10, pharmName: 'ali', price: 100, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 11', description: ['comp 1'], id: 11, pharmName: 'koky', price: 2, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 12', description: ['comp 1'], id: 12, pharmName: 'hader', price: 555, imgPharm: './logo.png' },
-      { imgUrl: './logo.png', name: 'Card 13', description: ['comp 1'], id: 13, pharmName: 'koko', price: 9080, imgPharm: './logo.png' }
+      {
+        imgUrl: './logo.png',
+        name: 'Card 1',
+        description: ['comp 1'],
+        id: 1,
+        pharmName: 'keky',
+        price: 2000,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 2',
+        description: ['comp 1'],
+        id: 2,
+        pharmName: 'tarshoby',
+        price: 1500,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 3',
+        description: ['comp 1'],
+        id: 3,
+        pharmName: 'almeldin',
+        price: 4000,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 4',
+        description: ['comp 1'],
+        id: 4,
+        pharmName: 'weso',
+        price: 1926,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 5',
+        description: ['comp 1'],
+        id: 5,
+        pharmName: 'khaled',
+        price: 400,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 6',
+        description: ['comp 1'],
+        id: 6,
+        pharmName: 'mahdy',
+        price: 100,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 7',
+        description: ['comp 1'],
+        id: 7,
+        pharmName: 'rewan',
+        price: 600,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 8',
+        description: ['comp 1'],
+        id: 8,
+        pharmName: 'emo',
+        price: 800,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 9',
+        description: ['comp 1'],
+        id: 9,
+        pharmName: 'roby',
+        price: 500,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 10',
+        description: ['comp 1'],
+        id: 10,
+        pharmName: 'ali',
+        price: 100,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 11',
+        description: ['comp 1'],
+        id: 11,
+        pharmName: 'koky',
+        price: 2,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 12',
+        description: ['comp 1'],
+        id: 12,
+        pharmName: 'hader',
+        price: 555,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
+      {
+        imgUrl: './logo.png',
+        name: 'Card 13',
+        description: ['comp 1'],
+        id: 13,
+        pharmName: 'koko',
+        price: 9080,
+        imgPharm: './logo.png',
+        isFlipped: false,
+      },
     ];
 
-    const max = Math.max(...this.allPackages.map(p => p.price));
+    const max = Math.max(...this.allPackages.map((p) => p.price));
     this.priceService.setMaxPrice(max);
 
-    const uniquePharmNames = [...new Set(this.allPackages.map(p => p.pharmName))];
+    const uniquePharmNames = [
+      ...new Set(this.allPackages.map((p) => p.pharmName)),
+    ];
     this.pharmNameService.setPharmNames(uniquePharmNames);
 
-    this.priceService.selectedPrice.subscribe(price => {
+    this.priceService.selectedPrice.subscribe((price) => {
       this.selectedPrice = price;
       this.filterPackages();
     });
 
-    this.pharmNameService.selectedPharmName$.subscribe(name => {
+    this.pharmNameService.selectedPharmName$.subscribe((name) => {
       this.selectedPharmName = name;
       this.filterPackages();
     });
@@ -61,9 +180,11 @@ export class DisplayPackagesComponent {
   }
 
   filterPackages() {
-    this.filteredPackages = this.allPackages.filter(pkg => {
+    this.filteredPackages = this.allPackages.filter((pkg) => {
       const matchesPrice = pkg.price <= this.selectedPrice;
-      const matchesPharm = this.selectedPharmName ? pkg.pharmName === this.selectedPharmName : true;
+      const matchesPharm = this.selectedPharmName
+        ? pkg.pharmName === this.selectedPharmName
+        : true;
       return matchesPrice && matchesPharm;
     });
     this.currentPage = 1;
