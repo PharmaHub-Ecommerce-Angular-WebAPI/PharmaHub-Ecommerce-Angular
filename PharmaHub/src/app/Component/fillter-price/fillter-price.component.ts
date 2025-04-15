d359202b2029dc536e2b1fc0d110df75e5c7b00b
wesariad <wesoryad2@gmail.com>
2025-04-15 21:15:24 +0200

    Add FillterPriceComponent with slider functionality for price filtering

---
+++ PharmaHub/src/app/Component/fillter-price/fillter-price.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MaxpriceService } from '../../services/maxprice.service';
+
+@Component({
+  selector: 'app-fillter-price',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './fillter-price.component.html',
+  styleUrl: './fillter-price.component.css'
+})
+export class FillterPriceComponent implements OnInit {
+  sliderValue: number = 0;
+  maxPrice: number = 100;
+
+  constructor(private priceService: MaxpriceService) {}
+
+  ngOnInit(): void {
+    this.priceService.maxPrice.subscribe(price => {
+      this.maxPrice = price;
+      this.sliderValue = price;
+    });
+  }
+
+  onSliderChange() {
+    this.priceService.updateSelectedPrice(this.sliderValue);
+  }
+}
