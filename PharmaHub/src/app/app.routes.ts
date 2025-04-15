00f42d84b9b7f6cad95c5a13b043c033ea5019e1
RouQandel <rawanqandel62@gmail.com>
2025-04-15 20:18:30 +0200

    Refactor component imports and routing for consistency and clarity

--- PharmaHub/src/app/app.routes.ts
+++ PharmaHub/src/app/app.routes.ts
@@ -1,9 +1,11 @@
-import { CustomerComponent } from "./Component/customer/customer.component";
-import { HomeComponent } from "./Component/Home/home.component";
+// Import the necessary modules and components
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./component/home/home.component";
+import { CustomerComponent } from "./component/customer/customer.component";
+
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent} ,
-    {path: 'customer' , component : CustomerComponent}
+    {path: 'home', component: HomeComponent } 
+    ,{path: 'customer' , component : CustomerComponent }
 ]; 
