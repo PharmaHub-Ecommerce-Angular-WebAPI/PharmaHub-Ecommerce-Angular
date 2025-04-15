36941427f0f1f2f73052c2558c8e834b823b9121
wesariad <wesoryad2@gmail.com>
2025-04-15 21:09:54 +0200

    Add routes for AllOffersComponent and AllPackagesComponent

--- PharmaHub/src/app/app.routes.ts
+++ PharmaHub/src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { AllOffersComponent } from "./Component/all-offers/all-offers.component";
+import { AllPackagesComponent } from "./Component/all-packages/all-packages.component";
 import { CustomerComponent } from "./Component/customer/customer.component";
 import { HomeComponent } from "./Component/Home/home.component";
 import { Routes } from '@angular/router';
@@ -5,5 +7,9 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent} ,
-    {path: 'customer' , component : CustomerComponent}
+    {path: 'customer' , component : CustomerComponent} ,
+    {path: 'allpackages' , component :AllPackagesComponent } ,
+    {path: 'alloffers' , component :AllOffersComponent } ,
+    
+
 ]; 
