6c5e306f1700350a22ec83ae84b81b8bd401b8b1
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-18 16:52:28 +0200

    Add body structure and import missing components in CustomerComponent

--- PharmaHub/src/app/Component/customer/customer.component.ts
+++ PharmaHub/src/app/Component/customer/customer.component.ts
@@ -3,6 +3,10 @@ import { PackagesComponent } from '../packages/packages.component';
 import { OffersComponent } from '../offers/offers.component';
 import { CustomerHeroComponent } from '../customer-hero/customer-hero.component';
 import { PharmaciesComponent } from '../pharmacies/pharmacies.component';
+import { PackageHeadingComponent } from '../package-heading/package-heading.component';
+import { PharmaciesEntryComponent } from '../pharmacies-entry/pharmacies-entry.component';
+import { OffersEnteryComponent } from '../offers-entery/offers-entery.component';
+import { FooterMarqueeComponent } from '../footer-marquee/footer-marquee.component';
 
 @Component({
   selector: 'app-customer',
@@ -11,6 +15,10 @@ import { PharmaciesComponent } from '../pharmacies/pharmacies.component';
     OffersComponent,
     CustomerHeroComponent,
     PharmaciesComponent,
+    PackageHeadingComponent,
+    PharmaciesEntryComponent,
+    OffersEnteryComponent,
+    FooterMarqueeComponent,
   ],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css',
