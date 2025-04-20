db015477c1835d2c7197aa53a7ed732e084cd04b
eman149 <emanabdou506@gmail.com>
2025-04-20 21:44:24 +0200

    Add pharmacy-related routes: include components for pharmacy info, product management, and nested routing.

--- PharmaHub/src/app/app.routes.ts
+++ PharmaHub/src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { HomeComponent } from './Component/Home/home.component';
 
 import { Routes } from '@angular/router';
 import { ViewAllPharmaciesComponent } from './Component/customer/view-all-pharmacies/view-all-pharmacies.component';
+import { DashPharmaciesComponent } from './Component/dash-pharmacies/dash-pharmacies.component';
+import { AddproductComponent } from './Component/dash-pharmacies/addproduct/addproduct.component';
+import { UpdateproductComponent } from './Component/dash-pharmacies/updateproduct/updateproduct.component';
+import { DeleteproductComponent } from './Component/dash-pharmacies/deleteproduct/deleteproduct.component';
+import { PharmacyInfoComponent } from './Component/dash-pharmacies/pharmacy-info/pharmacy-info.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -14,4 +19,19 @@ export const routes: Routes = [
   { path: 'allpackages', component: AllPackagesComponent },
   { path: 'alloffers', component: AllOffersComponent },
   { path: 'allpharmacies', component: ViewAllPharmaciesComponent },
+  { path: 'pharmacyinfo', component: PharmacyInfoComponent },
+  
+  { path: 'pharmacy', component: DashPharmaciesComponent,
+    children: [
+      {path: '' , redirectTo: 'pharmacyinfo' , pathMatch: 'full'} ,
+      {path: 'pharmacyinfo' , component: PharmacyInfoComponent} ,
+      {path: 'addproduct' , component: AddproductComponent} ,
+
+      {path: 'updateproduct' , component: UpdateproductComponent} ,
+      {path: 'deleteproduct' , component: DeleteproductComponent} ,
+
+
+
+    ]
+   },
 ];
