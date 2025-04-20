f2a90615fdbd1eaf9fc8e82f731afd83edb03b06
eman149 <emanabdou506@gmail.com>
2025-04-20 21:55:24 +0200

    Add initial implementation of UpdateproductComponent

---
+++ PharmaHub/src/app/Component/dash-pharmacies/updateproduct/updateproduct.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-updateproduct',
+  imports: [],
+  templateUrl: './updateproduct.component.html',
+  styleUrl: './updateproduct.component.css'
+})
+export class UpdateproductComponent {
+
+}
