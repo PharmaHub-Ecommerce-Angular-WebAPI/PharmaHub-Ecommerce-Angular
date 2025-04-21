71fe71fffdf898f9bac96d2d9cad1447c5a99509
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-21 11:19:48 +0200

    Refactor All Offers component structure and styles for improved layout and consistency

--- PharmaHub/src/app/Component/customer/all-offers/all-offers.component.ts
+++ PharmaHub/src/app/Component/customer/all-offers/all-offers.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { DisplayOffersComponent } from "../display-offers/display-offers.component";
+import { DisplayOffersComponent } from '../display-offers/display-offers.component';
 import { FillterPriceComponent } from '../fillter-price/fillter-price.component';
 import { FillterPHnameComponent } from '../fillter-phname/fillter-phname.component';
+import { MoveUpAnimateDirective } from '../../../Directives/move-up-animate.directive';
 
 @Component({
   selector: 'app-all-offers',
-  imports: [DisplayOffersComponent, FillterPriceComponent , FillterPHnameComponent],
+  imports: [
+    DisplayOffersComponent,
+    FillterPriceComponent,
+    FillterPHnameComponent,
+    MoveUpAnimateDirective,
+  ],
   templateUrl: './all-offers.component.html',
-  styleUrl: './all-offers.component.css'
+  styleUrl: './all-offers.component.css',
 })
-export class AllOffersComponent {
-
-}
+export class AllOffersComponent {}
