99df3acbe4ffc7f867d2fc23220ab88460000e67
eman149 <emanabdou506@gmail.com>
2025-04-22 01:40:33 +0200

    Refactor DisplayOffersComponent to remove commented-out code and improve clarity

--- PharmaHub/src/app/Component/customer/display-offers/display-offers.component.ts
+++ PharmaHub/src/app/Component/customer/display-offers/display-offers.component.ts
@@ -31,221 +31,8 @@ export class DisplayOffersComponent {
     private pharmNameService: PharmNameService ,
         private apiService: ApiProductService
     
-  ) {
-    // this.offers = [
-    //   {
-    //     imgUrl: './logo.png',
-    //     name: 'Card 1',
-    //     description: [
-    //       'This is compnet 1',
-    //       'This is compnet 2',
-    //       'This is compnet 3',
-    //     ],
-    //     id: 1,
-    //     pharmName: 'khaled',
-    //     price: 1000,
-    //     imgPharm: './logo.png',
-    //     DissPrice: 0,
-    //     isFlipped: false,
-    //   },
-    //   {
-    //     imgUrl: './logo.png',
-    //     name: 'Card 2',
-    //     description: [
-    //       'This is compnet 1',
-    //       'This is compnet 2',
-    //       'This is compnet 3',
-    //     ],
-    //     id: 1,
-    //     pharmName: 'weso',
-    //     price: 2000,
-    //     imgPharm: './logo.png',
-    //     DissPrice: 20,
-    //     isFlipped: false,
-    //   },
-    //   {
-    //     imgUrl: './logo.png',
-    //     name: 'Card 3',
-    //     description: [
-    //       'This is compnet 1',
-    //       'This is compnet 2',
-    //       'This is compnet 3',
-    //     ],
-    //     id: 1,
-    //     pharmName: 'roro',
-    //     price: 3000,
-    //     imgPharm: './logo.png',
-    //     DissPrice: 0,
-    //     isFlipped: false,
-    //   },
-    //   {
-    //     imgUrl: './logo.png',
-    //     name: 'Card 4',
-    //     description: [
-    //       'This is compnet 1',
-    //       'This is compnet 2',
-    //       'This is compnet 3',
-    //     ],
-    //     id: 1,
-    //     pharmName: 'mahdy',
-    //     price: 4000,
-    //     imgPharm: './logo.png',
-    //     DissPrice: 20,
-    //     isFlipped: false,
-    //   },
-    //   {
-    //     imgUrl: './logo.png',
-    //     name: 'Card 5',
-    //     description: [
-    //       'This is compnet 1',
-    //       'This is compnet 2',
-    //       'This is compnet 3',
-    //     ],
-    //     id: 1,
-    //     pharmName: 'emo',
-    //     price: 5000,
-    //     imgPharm: './logo.png',
-    //     DissPrice: 20,
-    //     isFlipped: false,
-    //   },
-    //   {
-    //     imgUrl: './logo.png',
-    //     name: 'Card 6',
-    //     description: [
-    //       'This is compnet 1',
-    //       'This is compnet 2',
-    //       'This is compnet 3',
-    //     ],
-    //     id: 1,
-    //     pharmName: 'weso',
-    //     price: 6000,
-    //     imgPharm: './logo.png',
-    //     DissPrice: 20,
-    //     isFlipped: false,
-    //   },
-    //   {
-    //     imgUrl: './logo.png',
-    //     name: 'Card 7',
-    //     description: [
-    //       'This is compnet 1',
-    //       'This is compnet 2',
-    //       'This is compnet 3',
-    //     ],
-    //     id: 1,
-    //     pharmName: 'koky',
-    //     price: 7000,
-    //     imgPharm: './logo.png',
-    //     DissPrice: 20,
-    //     isFlipped: false,
-    //   },
-    //   {
-    //     imgUrl: './logo.png',
-    //     name: 'Card 8',
-    //     description: [
-    //       'This is compnet 1',
-    //       'This is compnet 2',
-    //       'This is compnet 3',
-    //     ],
-    //     id: 1,
-    //     pharmName: 'soso',
-    //     price: 8000,
-    //     imgPharm: './logo.png',
-    //     DissPrice: 20,
-    //     isFlipped: false,
-    //   },
-    //   {
-    //     imgUrl: './logo.png',
-    //     name: 'Card 9',
-    //     description: [
-    //       'This is compnet 1',
-    //       'This is compnet 2',
-    //       'This is compnet 3',
-    //     ],
-    //     id: 1,
-    //     pharmName: 'koko',
-    //     price: 9000,
-    //     imgPharm: './logo.png',
-    //     DissPrice: 20,
-    //     isFlipped: false,
-    //   },
-    //   {
-    //     imgUrl: './logo.png',
-    //     name: 'Card 10',
-    //     description: [
-    //       'This is compnet 1',
-    //       'This is compnet 2',
-    //       'This is compnet 3',
-    //     ],
-    //     id: 1,
-    //     pharmName: 'weso',
-    //     price: 500,
-    //     imgPharm: './logo.png',
-    //     DissPrice: 20,
-    //     isFlipped: false,
-    //   },
-    // ];
-
-    // const max = Math.max(...this.offers.map((p) => p.price));
-    // this.priceService.setMaxPrice(max);
-
-    // const uniquePharmNames = [...new Set(this.offers.map((p) => p.pharmName))];
-    // this.pharmNameService.setPharmNames(uniquePharmNames);
-
-    // this.priceService.selectedPrice.subscribe((price) => {
-    //   this.selectedPrice = price;
-    //   this.filterPackages();
-    // });
-
-    // this.pharmNameService.selectedPharmName$.subscribe((name) => {
-    //   this.selectedPharmName = name;
-    //   this.filterPackages();
-    // });
-
-    // this.filteredPackages = [...this.offers];
-  }
-
-  // ngOnInit(): void {
-  //   this.apiService.getOffers().subscribe((response: any[]) => {
-  //     this.offers = response.map((pkg) => ({
-  //       id: pkg.id,
-  //       name: pkg.name,
-  //       imgUrl: pkg.imageUrl,
-  //       description: pkg.packageComponents,
-  //       pharmName: pkg.pharmacyName,
-  //       imgPharm: pkg.pharmacyLogo,
-  //       price: pkg.price,
-  //       isFlipped: false,
-  //       DissPrice: pkg.discountRate,
-  //     }));
-      
-
-  //     // Set max price for slider
-  //     const max = Math.max(...this.offers.map((p) => p.price));
-  //     this.priceService.setMaxPrice(max);
-
-  //     // Set unique pharmacy names
-  //     const uniquePharmNames = [
-  //       ...new Set(this.offers.map((p) => p.pharmName)),
-  //     ];
-  //     this.pharmNameService.setPharmNames(uniquePharmNames);
-
-  //     // Apply initial filters
-  //     this.filteredPackages = [...this.offers];
-  //   });
-
-  //   // Listen for filters
-  //   this.priceService.selectedPrice.subscribe((price) => {
-  //     this.selectedPrice = price;
-  //     this.filterPackages();
-  //   });
-
-  //   this.pharmNameService.selectedPharmName$.subscribe((name) => {
-  //     this.selectedPharmName = name;
-  //     this.filterPackages();
-  //   });
-
-  //   console.log( this.offers)
-  // }
+  ) {}
+    
   ngOnInit(): void {
     // جلب العروض
     this.apiService.getOffers().subscribe((response: any[]) => {
@@ -261,30 +48,30 @@ export class DisplayOffersComponent {
         DissPrice: pkg.discountRate,
       }));
   
-      // استخراج أسماء الصيدليات
+     
       const uniquePharmNames = [
         ...new Set(this.offers.map((p) => p.pharmName)),
       ];
       this.pharmNameService.setPharmNames(uniquePharmNames);
   
-      // تعيين العروض كـ filteredPackages مبدئيًا
+      
       this.filteredPackages = [...this.offers];
     });
   
-    // ✅ جلب الماكس برايس من API
+    
     this.apiService.getmaxpriceOffers().subscribe((res: any) => {
       if (res && res.maxPrice) {
         this.priceService.setMaxPrice(res.maxPrice);
       }
     });
   
-    // فلترة بالسعر
+  
     this.priceService.selectedPrice.subscribe((price) => {
       this.selectedPrice = price;
       this.filterPackages();
     });
   
-    // فلترة باسم الصيدلية
+   
     this.pharmNameService.selectedPharmName$.subscribe((name) => {
       this.selectedPharmName = name;
       this.filterPackages();
@@ -319,25 +106,3 @@ export class DisplayOffersComponent {
   }
 }
 
-//     const max = Math.max(...this.offers.map(p => p.price));
-//     this.priceService.setMaxPrice(max);
-
-//     this.priceService.selectedPrice.subscribe(value => {
-//       this.filteredPackages = this.offers.filter(p => p.price <= value);
-//       this.currentPage = 1;
-//     });
-//   }
-
-//   get totalPages(): number {
-//     return Math.ceil(this.filteredPackages.length / this.cardsPerPage);
-//   }
-
-//   get visibleCards() {
-//     const start = (this.currentPage - 1) * this.cardsPerPage;
-//     return this.filteredPackages.slice(start, start + this.cardsPerPage);
-//   }
-
-//   setPage(page: number) {
-//     this.currentPage = page;
-//   }
-// }
