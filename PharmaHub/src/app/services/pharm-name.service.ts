beaf6c57396013c47ed893029a920c831a9cd1d2
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-21 09:33:19 +0200

    Fix formatting in PharmNameService for consistency

--- PharmaHub/src/app/services/pharm-name.service.ts
+++ PharmaHub/src/app/services/pharm-name.service.ts
@@ -2,22 +2,19 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PharmNameService {
   private selectedPharmNameSource = new BehaviorSubject<string | null>(null);
   selectedPharmName$ = this.selectedPharmNameSource.asObservable();
 
-
   private pharmNamesSource = new BehaviorSubject<string[]>([]);
   pharmNames$ = this.pharmNamesSource.asObservable();
 
- 
   setSelectedPharmName(name: string | null) {
     this.selectedPharmNameSource.next(name);
   }
 
- 
   setPharmNames(names: string[]) {
     this.pharmNamesSource.next(names);
   }
