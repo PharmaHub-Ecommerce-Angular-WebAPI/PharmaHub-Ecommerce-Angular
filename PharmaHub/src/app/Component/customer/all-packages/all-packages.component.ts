22f9bd2a27ebbacecd8963937dd4ed1289eb5071
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-21 11:20:16 +0200

    Refactor All Packages component structure and styles for improved layout and consistency

--- PharmaHub/src/app/Component/customer/all-packages/all-packages.component.ts
+++ PharmaHub/src/app/Component/customer/all-packages/all-packages.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FillterPriceComponent } from '../fillter-price/fillter-price.component';
 import { DisplayPackagesComponent } from '../display-packages/display-packages.component';
 import { FillterPHnameComponent } from '../fillter-phname/fillter-phname.component';
+import { MoveUpAnimateDirective } from '../../../Directives/move-up-animate.directive';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-all-packages',
@@ -9,6 +11,8 @@ import { FillterPHnameComponent } from '../fillter-phname/fillter-phname.compone
     FillterPriceComponent,
     DisplayPackagesComponent,
     FillterPHnameComponent,
+    MoveUpAnimateDirective,
+    RouterModule,
   ],
   templateUrl: './all-packages.component.html',
   styleUrl: './all-packages.component.css',
