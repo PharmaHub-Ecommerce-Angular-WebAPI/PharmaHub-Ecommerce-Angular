d7420d22a10de8f73de7a64215c9d4306db57f52
wesariad <wesoryad2@gmail.com>
2025-04-23 01:41:24 +0200

    Refactor login and register component: enhance login method and improve code structure

--- PharmaHub/src/app/Component/login-and-register-customer/login-and-register-customer.component.ts
+++ PharmaHub/src/app/Component/login-and-register-customer/login-and-register-customer.component.ts
@@ -2,16 +2,44 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MoveUpAnimateDirective } from '../../Directives/move-up-animate.directive';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-and-register-customer',
   imports: [CommonModule, FormsModule, MoveUpAnimateDirective, RouterModule],
+  standalone: true,
   templateUrl: './login-and-register-customer.component.html',
   styleUrl: './login-and-register-customer.component.css',
 })
 export class LoginAndRegisterCustomerComponent {
+  //// formdata 
   loginemail: string = '';
   loginPassword: string = '';
   confirmPassword: string = '';
+
+  constructor(private authService: AuthService , private router: Router) {
+  }
+  
+  
+
+  login() {
+    const loginData = {
+      email: this.loginemail,
+      password: this.loginPassword
+    };
+  
+    this.authService.login(loginData).subscribe({
+      next: (response) => {
+        console.log('Login successful!', response);
+  
+        this.router.navigate(['customer']);
+      },
+      error: (error) => {
+        console.error('Login error:', error);
+        alert('Invalid email or password.');
+      }
+    });
+  }
+  
 }
