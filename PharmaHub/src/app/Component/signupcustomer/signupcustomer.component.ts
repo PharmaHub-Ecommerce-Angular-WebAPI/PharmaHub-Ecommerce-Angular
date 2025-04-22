fdbbef164f10bc042dd730431702c450fd41c1e6
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-22 17:35:15 +0200

    Add signup customer component with HTML, CSS, and unit tests

---
+++ PharmaHub/src/app/Component/signupcustomer/signupcustomer.component.ts
@@ -0,0 +1,60 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MoveUpAnimateDirective } from '../../Directives/move-up-animate.directive';
+
+@Component({
+  selector: 'app-signupcustomer',
+  imports: [CommonModule, RouterModule, FormsModule, MoveUpAnimateDirective],
+  templateUrl: './signupcustomer.component.html',
+  styleUrl: './signupcustomer.component.css',
+})
+export class SignupcustomerComponent {
+  signupemail: string = '';
+  signupPassword: string = '';
+  confirmPassword: string = '';
+  location: string = '';
+  selectedCountry: string = '';
+  selectedCity: string = '';
+  countries = [
+    {
+      name: 'Egypt',
+      cities: [
+        'Cairo',
+        'Alexandria',
+        'Giza',
+        'Port Said',
+        'Suez',
+        'Dakahlia',
+        'Sharqia',
+        'Qalyubia',
+        'Kafr El Sheikh',
+        'Gharbia',
+        'Monufia',
+        'Beheira',
+        'Ismailia',
+        'Gharbia',
+        'Faiyum',
+        'Beni Suef',
+        'Minya',
+        'Asyut',
+        'Sohag',
+        'Qena',
+        'Luxor',
+        'Aswan',
+        'Red Sea',
+        'New Valley',
+        'Matrouh',
+        'North Sinai',
+        'South Sinai',
+      ],
+    },
+  ];
+  filteredCities: string[] = [];
+  onCountryChange(): void {
+    const country = this.countries.find((c) => c.name === this.selectedCountry);
+    this.filteredCities = country ? country.cities : [];
+    this.selectedCity = ''; // reset city
+  }
+}
