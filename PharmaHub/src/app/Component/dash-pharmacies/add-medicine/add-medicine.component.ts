95ad1c15c2ea8e985f89196298c0c0ab8f3043f9
eman149 <emanabdou506@gmail.com>
2025-04-23 08:12:47 +0200

    Add AddMedicineComponent with initial implementation

---
+++ PharmaHub/src/app/Component/dash-pharmacies/add-medicine/add-medicine.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-medicine',
+  imports: [],
+  templateUrl: './add-medicine.component.html',
+  styleUrl: './add-medicine.component.css'
+})
+export class AddMedicineComponent {
+
+}
