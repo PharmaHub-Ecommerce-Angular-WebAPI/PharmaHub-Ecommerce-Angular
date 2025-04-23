6f68714e3885e8554ff6b6b3706a172149ace163
eman149 <emanabdou506@gmail.com>
2025-04-23 08:11:10 +0200

    Add AddHealthDeviceComponent with initial structure

---
+++ PharmaHub/src/app/Component/dash-pharmacies/add-health-device/add-health-device.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-health-device',
+  imports: [],
+  templateUrl: './add-health-device.component.html',
+  styleUrl: './add-health-device.component.css'
+})
+export class AddHealthDeviceComponent {
+
+}
