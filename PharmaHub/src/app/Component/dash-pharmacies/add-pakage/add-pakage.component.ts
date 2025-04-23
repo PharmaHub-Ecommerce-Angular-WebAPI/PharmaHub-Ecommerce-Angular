0ce2719b66fd1b3a9fcfee4cdecf5fcd93d28fa1
eman149 <emanabdou506@gmail.com>
2025-04-23 08:14:11 +0200

    Add AddPakageComponent with initial implementation

---
+++ PharmaHub/src/app/Component/dash-pharmacies/add-pakage/add-pakage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-pakage',
+  imports: [],
+  templateUrl: './add-pakage.component.html',
+  styleUrl: './add-pakage.component.css'
+})
+export class AddPakageComponent {
+
+}
