197a39137b18a61a965405900b7cb3cfa985e820
wesariad <wesoryad2@gmail.com>
2025-04-23 23:59:42 +0200

    Enhance add-beauty-product component with product form, image upload handling, and validation

--- PharmaHub/src/app/Component/dash-pharmacies/add-beauty-product/add-beauty-product.component.ts
+++ PharmaHub/src/app/Component/dash-pharmacies/add-beauty-product/add-beauty-product.component.ts
@@ -1,12 +1,128 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ApiProductService } from '../../../services/api-product.service';
+import { CommonModule } from '@angular/common';
+import { ProductUpdateService } from '../../../services/product-update.service';
 
 @Component({
   selector: 'app-add-beauty-product',
-  imports: [],
+  imports: [    FormsModule, CommonModule ],
+    providers: [ApiProductService],
+  
   templateUrl: './add-beauty-product.component.html',
   styleUrl: './add-beauty-product.component.css'
 })
 export class AddBeautyProductComponent {
+  constructor(private apiProductService: ApiProductService ,  private productUpdateService: ProductUpdateService) {}
+
+  product = {
+    name: '',
+    description: '',
+    price: 0,
+    quantity: 0,
+    strength: 0,
+    category: '',
+    pharmacyId: '',
+    components: [] as string[],
+    logoFile: null as File | null, 
+  };
+  previewImage: string | null = null;
+  addComponent() {
+    this.product.components.push('');
+  }
+  
+  removeComponent(index: number) {
+    this.product.components.splice(index, 1);
+  }
+  
+  // onImageSelected(event: any) {
+  //   const file = event.target.files[0];
+  //   if (file) {
+  //     this.product.imageFile = file;
+  //   }
+  // }
+  // Handle the file change event
+  logoError: 'required' | 'invalidType' | 'tooLarge' | null = null;
+  logoTouched = false;
+
+  onImageSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.logoTouched = true;
+  
+    if (!input.files || input.files.length === 0) {
+      this.product.logoFile = null;
+      this.logoError = 'required';
+      return;
+    }
+  
+    const file = input.files[0];
+    const reader = new FileReader();
+  
+    reader.onload = () => {
+    
+      this.previewImage = reader.result as string;
+    };
+  
+    reader.readAsDataURL(file);
+  
+    const validTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+  
+    if (!validTypes.includes(file.type)) {
+      this.product.logoFile = null;
+      this.logoError = 'invalidType';
+      return;
+    }
+  
+    const maxSizeMB = 5;
+    if (file.size > maxSizeMB * 1024 * 1024) {
+      this.product.logoFile = null;
+      this.logoError = 'tooLarge';
+      return;
+    }
+  
+    this.product.logoFile = file;
+  }
+  submitProduct() {
+    const formData = new FormData();
+    formData.append('Name', this.product.name);
+    formData.append('Description', this.product.description);
+    formData.append('Price', this.product.price.toString());
+    formData.append('Quantity', this.product.quantity.toString());
+    formData.append('Strength', '0'.toString()); 
+    // formData.append('pharmacyId', this.product.pharmacyId);
+    formData.append('Category', 'BeautyProduct');
+
+    const pharmacyId = localStorage.getItem('userId');
+    if (pharmacyId) {
+      formData.append('PharmacyId', pharmacyId);
+    } else {
+      console.warn('No pharmacyId found in localStorage');
+    }
+
+
+    if (this.product.logoFile) {
+      formData.append('ImageUrl', this.product.logoFile, this.product.logoFile.name);
+      // formData.append('ImageUrl', this.logoFile, this.logoFile.name);
+    }
+    // this.product.components.forEach((comp, index) => {
+    //   formData.append(`Components[${index}]`, comp);
+    // });
+    formData.append('Components', '');
+
+
+   console.log(' Form Data:', formData);
+    this.apiProductService.addproduct(formData).subscribe({
+      next: res => {
+        console.log('Product added successfully', res);
+
+        this.productUpdateService.notifyProductAdded();
+      },
+      error: err => {
+        console.error('Error adding product', err);
+      }
+    });
+  }
+  
   
 
 }
