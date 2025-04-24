b101e5952a319765615d1a2aba9233caac1b2bf5
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-24 08:01:36 +0200

    Add ProfilePersonalCare component with template and styles

---
+++ PharmaHub/src/app/Component/pharmacy-profile/profile-personal-care/profile-personal-care.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-personal-care',
+  imports: [],
+  templateUrl: './profile-personal-care.component.html',
+  styleUrl: './profile-personal-care.component.css'
+})
+export class ProfilePersonalCareComponent {
+
+}
