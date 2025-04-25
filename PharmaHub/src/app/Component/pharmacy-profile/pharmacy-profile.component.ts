ae63073a7e2da7286d73fce3a3303458e05bc077
wesariad <wesoryad2@gmail.com>
2025-04-26 01:12:55 +0300

    Refactor PharmacyProfileComponent to enhance API integration and remove unused pharmacy data

--- PharmaHub/src/app/Component/pharmacy-profile/pharmacy-profile.component.ts
+++ PharmaHub/src/app/Component/pharmacy-profile/pharmacy-profile.component.ts
@@ -1,31 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IPharmacies } from '../../Models/ipharmacies';
-import { RouterLink, RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
+import { ApiPharmacyService } from '../../services/api-pharmacy.service';
+import { Iphprofile } from '../../Models/iphprofile';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pharmacy-profile',
-  imports: [CommonModule, RouterModule, RouterLink],
+  imports: [CommonModule, RouterModule, RouterLink , FormsModule],
   templateUrl: './pharmacy-profile.component.html',
   styleUrl: './pharmacy-profile.component.css',
 })
 export class PharmacyProfileComponent {
+
   imgUrl1: string = './logo.png';
   imgUrl2: string = './Pharmacies/1.png';
-  pharmacies: IPharmacies[];
-  constructor() {
-    this.pharmacies = [
-      {
-        id: '1',
-        Name: 'Pharmacy 1',
-        imgUrl: './assets/pharmacy1.jpg',
-      },
-      {
-        id: '2',
-        Name: 'Pharmacy 2',
+  pharmacy: Iphprofile | null = null;
 
-        imgUrl: './assets/pharmacy2.jpg',
-      },
-    ];
+  constructor(
+    private apiService: ApiPharmacyService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    // const id = '4767c059-06eb-4a16-9d5e-d274722a408a';
+    const id = localStorage.getItem('pharmacyId');
+    if (id) {
+      this.apiService.getPharmacyById(id).subscribe((pkg: Iphprofile) => {
+        this.pharmacy = pkg;
+        console.log(pkg);
+       // localStorage.removeItem('pharmacyId');  // مسح الـ id بعد عرض البيانات
+      });
+    }
+    
+    // this.apiService.getPharmacyById(id).subscribe((pkg: Iphprofile) => {
+    //   this.pharmacy = pkg;
+    //   console.log(pkg); // تحقق من أن البيانات تصل بشكل صحيح
+    // });
   }
 }
