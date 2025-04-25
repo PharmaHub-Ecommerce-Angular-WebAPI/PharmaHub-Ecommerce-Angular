3f477602d0c44aeeb3a7cd720732a4cdd88e0527
wesariad <wesoryad2@gmail.com>
2025-04-26 01:09:47 +0300

    Refactor ApiPharmacyService to streamline API calls and enhance pharmacy retrieval methods

--- PharmaHub/src/app/services/api-pharmacy.service.ts
+++ PharmaHub/src/app/services/api-pharmacy.service.ts
@@ -2,19 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../enviroments/enviroment.development';
 import { IPharmacies } from '../Models/ipharmacies';
+import { Iphprofile } from '../Models/iphprofile';
+import { map, Observable } from 'rxjs';
+import { Ip } from '../Models/ip';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiPharmacyService {
 
-   private apiUrl = `${environment.baseUrl}/api/Pharmacy/Getpharmacies`;
+  // private apiUrl = `${environment.baseUrl}/api/Pharmacy/Getpharmacies`;
   constructor(private httpclient: HttpClient) { 
 
   }
 
   getPharmacy() {
-      return this.httpclient.get<IPharmacies[]>(`${this.apiUrl}`, 
+      return this.httpclient.get<IPharmacies[]>(`${environment.baseUrl}/api/Pharmacy/Getpharmacies`, 
       //   {
       //   params: {
       //   }
@@ -22,7 +25,7 @@ export class ApiPharmacyService {
     );
     }
     getPharmacylimted() {
-      return this.httpclient.get<IPharmacies[]>(`${this.apiUrl}`, 
+      return this.httpclient.get<IPharmacies[]>(`${environment.baseUrl}/api/Pharmacy/Getpharmacies`, 
         {
         params: {
           numberOfUsers : 3 
@@ -30,4 +33,28 @@ export class ApiPharmacyService {
       }
     );
     }
+  
+    getPharmacyById(id: string):Observable<Iphprofile>   {
+      return this.httpclient.get<Iphprofile>(`${environment.baseUrl}/api/Pharmacy/GetPharmacyById/${id}`);
+}
+
+getSearchPharmacies(name: string): Observable<Ip[]> {
+  return this.httpclient.get<any[]>(`${environment.baseUrl}/api/Products/mini-search?name=${name}`).pipe(
+    map((products) => {
+      // فلترة الصيدليات الفريدة
+      const seen = new Set();
+      const uniquePharmacies = products.filter((item) => {
+        if (seen.has(item.pharmacyId)) return false;
+        seen.add(item.pharmacyId);
+        return true;
+      }).map((item) => ({
+        id: item.pharmacyId,
+        pharmacyName  : item.pharmacyName,
+        logoURL: item.pharmacyLogo
+      }));
+
+      return uniquePharmacies;
+    })
+  );
+}
 }
