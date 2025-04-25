6a33e0ac190522aa1f6c0f91d23ed085786d1709
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-04-26 00:55:05 +0300

    Home Page Edits to be continued

--- PharmaHub/src/app/Component/Home/home.component.ts
+++ PharmaHub/src/app/Component/Home/home.component.ts
@@ -1,19 +1,29 @@
-import { Component ,AfterViewInit,  CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { register } from 'swiper/element/bundle';
-
+import { FooterMarqueeComponent } from '../customer/footer-marquee/footer-marquee.component';
+import { MoveAnimateDirective } from '../../Directives/leftorrightfade.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule ],
+  imports: [
+    RouterModule,
+    FooterMarqueeComponent,
+    MoveAnimateDirective,
+    CommonModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent implements AfterViewInit {
   ngAfterViewInit() {
-  
     register();
-  }  
+  }
 }
