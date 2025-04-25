59559be04db4424b0070dd38915f3e3b4b91fa53
wesariad <wesoryad2@gmail.com>
2025-04-26 01:12:16 +0300

    Refactor ProfileHealthDevicesComponent to optimize API calls and improve package handling logic

--- PharmaHub/src/app/Component/pharmacy-profile/profile-health-devices/profile-health-devices.component.ts
+++ PharmaHub/src/app/Component/pharmacy-profile/profile-health-devices/profile-health-devices.component.ts
@@ -55,17 +55,12 @@ export class ProfileHealthDevicesComponent implements OnInit {
     alert('product added to cart');
   }
   ngOnInit(): void {
-    const packagesWithoutOffers$ = this.apiService.getPackages();
-    const packagesWithOffers$ = this.apiService.getpackgeslimeted();
+    const healthDevicePackages$ = this.apiService.getHealthdevicePHARMACY();
     const maxPrice$ = this.apiService.getmaxpricePackages();
-
-    forkJoin([
-      packagesWithoutOffers$,
-      packagesWithOffers$,
-      maxPrice$,
-    ]).subscribe(
-      ([nonOfferPackages, offerPackages, max]: [any[], any[], any]) => {
-        const formattedNonOffer = nonOfferPackages.map((pkg) => ({
+  
+    healthDevicePackages$.subscribe(
+      (healthDevicePackages: any[]) => {
+        const formattedHealthDevice = healthDevicePackages.map((pkg) => ({
           id: pkg.id,
           name: pkg.name,
           imgUrl: pkg.imageUrl,
@@ -75,43 +70,35 @@ export class ProfileHealthDevicesComponent implements OnInit {
           price: pkg.price,
           isFlipped: false,
         }));
-
-        const formattedOffers = offerPackages.map((offer) => ({
-          id: offer.id,
-          name: offer.name,
-          imgUrl: offer.imageUrl,
-          description: offer.packageComponents,
-          pharmName: offer.pharmacyName,
-          imgPharm: offer.pharmacyLogo,
-          price: offer.price,
-          discountRate: offer.discountRate,
-          isFlipped: false,
-        }));
-
-        this.allPackages = [...formattedNonOffer, ...formattedOffers];
-
-        const maxPriceFromApi = typeof max === 'number' ? max : parseFloat(max);
-        this.priceService.setMaxPrice(maxPriceFromApi);
-
-        const uniquePharmNames = [
-          ...new Set(this.allPackages.map((p) => p.pharmName)),
-        ];
-        this.pharmNameService.setPharmNames(uniquePharmNames);
-
-        this.filteredPackages = [...this.allPackages];
+  
+        this.allPackages = [...formattedHealthDevice];
+  
+        // جلب الحد الأقصى للسعر
+        maxPrice$.subscribe((max: any) => {
+          const maxPriceFromApi = typeof max === 'number' ? max : parseFloat(max);
+          this.priceService.setMaxPrice(maxPriceFromApi);
+  
+          const uniquePharmNames = [
+            ...new Set(this.allPackages.map((p) => p.pharmName)),
+          ];
+          this.pharmNameService.setPharmNames(uniquePharmNames);
+  
+          this.filteredPackages = [...this.allPackages];
+        });
       }
     );
-
+  
     this.priceService.selectedPrice.subscribe((price) => {
       this.selectedPrice = price;
       this.filterPackages();
     });
-
+  
     this.pharmNameService.selectedPharmName$.subscribe((name) => {
       this.selectedPharmName = name;
       this.filterPackages();
     });
   }
+  
 
   filterPackages() {
     this.filteredPackages = this.allPackages.filter((pkg) => {
