1a3659cfe4bc78ab44268d8185b20d81058272c3
Mohamedmahdy404 <mohamedmahdy3162@gmail.com>
2025-04-26 02:11:26 +0300

    Add OrderBox component and update routing for order management

---
+++ PharmaHub/src/app/Component/dash-pharmacies/order-box/order-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-box',
+  imports: [],
+  templateUrl: './order-box.component.html',
+  styleUrl: './order-box.component.css'
+})
+export class OrderBoxComponent {
+
+}
